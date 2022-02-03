d1e642992db7f0b0ca656dea9a174300130b9652
royweb <xytank@gmail.com>
2022-02-03 14:22:09 -0500

    Add bootstrap modal

--- main.js
+++ main.js
@@ -2,7 +2,6 @@
 Moralis.initialize("yOGw5F1DxY2eA9ByFSlvoze95s9woOjLcHzxgm9Y");
 Moralis.serverURL = "https://okraypjofzst.usemoralis.com:2053/server"
 
-/** Add from here down */
 async function login() {
   let user = Moralis.User.current();
   if (!user) {
@@ -16,4 +15,9 @@ async function login() {
   }
 }
 
+function openModal() {
+  document.getElementById("token_modal").style.display = "block";
+}
+
+document.getElementById("from_token_select").onclick = openModal;
 document.getElementById("login_button").onclick = login;
