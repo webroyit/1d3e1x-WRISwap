6a19426f440793a1052bc9ab9b1ad06408d067b3
royweb <xytank@gmail.com>
2022-02-04 13:27:39 -0500

    Fetch all tokens using 1Inch plugin

--- main.js
+++ main.js
@@ -2,6 +2,19 @@
 Moralis.initialize("yOGw5F1DxY2eA9ByFSlvoze95s9woOjLcHzxgm9Y");
 Moralis.serverURL = "https://okraypjofzst.usemoralis.com:2053/server"
 
+async function init() {
+  await Moralis.initPlugins();
+  await Moralis.enable();
+  await listAvailableTokens();
+}
+
+async function listAvailableTokens() {
+  const tokens = await Moralis.Plugins.oneInch.getSupportedTokens({
+    chain: 'eth', // The blockchain you want to use (eth/bsc/polygon)
+  });
+  console.log(tokens);
+}
+
 async function login() {
   let user = Moralis.User.current();
   if (!user) {
@@ -22,6 +35,8 @@ function openModal() {
 function closeModal() {
   document.getElementById("token_modal").style.display = "none";
 }
+
+init();
 
 document.getElementById("from_token_select").onclick = openModal;
 document.getElementById("modal_close").onclick = closeModal;
